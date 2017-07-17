ff05546161718128444008ed1aff0a2ddf31c7cb
Arturo Cobos <arturo@co-boss.com>
2017-07-18 01:05:25 +0300

    Correciones

--- holaWeb.js
+++ holaWeb.js
@@ -1,14 +1,26 @@
 var restify= require('restify');
-var botbuilder = require('botbuilder');
+var builder = require('botbuilder');
 
 var server = restify.createServer();
-server.listen(process.env.port || porcess.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s ',server.name, server.url);
 });
+// No te preocupes por estas credenciales por ahora, luego las usaremos para conectar los canales.
 var connector = new builder.ChatConnector({
-
     appId: '',
     appPassword: ''
 });
-var bot = new builder.universalBot(connector);
-server.post('/api/messsages', connector.listen());
+// Ahora utilizamos un UniversalBot
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Dialogos
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, '¿Cómo te llamas?');
+    },
+    function (session, results) {
+        let msj = results.response;
+        session.send(`Hola ${msj}!`);
+    }
+]);
